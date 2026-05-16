f27743e81091994616ce3745ee850abc29cf68f3
Bruno Silva <bpinheiroms@gmail.com>
2026-05-16 11:58:52 -0300

    feat: enable snacks neovim plugins

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = true },
+      explorer = { enabled = true },
+      image = { enabled = true },
+      input = { enabled = true },
+      notifier = {
+        enabled = true,
+        timeout = 3000,
+      },
+      picker = { enabled = true },
+      quickfile = { enabled = true },
+      words = { enabled = true },
+    },
+    keys = {
+      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
+      { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Search Word" },
+      { "<leader>un", function() Snacks.picker.notifications() end, desc = "Notification History" },
+    },
+  },
+}
