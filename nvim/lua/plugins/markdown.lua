72b1f5c182bc883a3c572cbc1be8227731fd63e9
Bruno Pinheiro <bpinheiroms@gmail.com>
2026-05-18 13:39:17 -0300

    feat: update cmux and markdown setup (#25)

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npx --yes yarn install",
+  },
+  {
     "mfussenegger/nvim-lint",
     opts = function(_, opts)
       opts.linters_by_ft = opts.linters_by_ft or {}
